76242a235eb06afd1b589e9133a0186ffd8d3581
Diante Hazelle-Mclean <100953671@georgebrown.ca>
2025-04-06 03:42:57 +0000

    added hover animation

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Card.css'; 
+import { FaLinkedin, FaGithub, FaBehance } from 'react-icons/fa';
 
 const Card = () => {
   return (
@@ -18,22 +19,33 @@ const Card = () => {
 			<div class="job">Web Developer & Graphic Designer</div>
 			
 			<div class="actions">
+      <a href="https://diantes-design-portfolio.webflow.io/" target="_blank">
 				<button class="btn">Portfolio</button>
+        </a>
+        <a href="https://drive.google.com/file/d/1V85fj8EcFEOougXok9Hct2_ltsEyGpGt/view" target="_blank">
 				<button class="btn">Resume</button>
+        </a>
 			</div>
 		</div>
 		
 		<div class="stats">
 			<div class="box">
-				<span class="value">523</span>
+      <a href="https://www.linkedin.com/in/diante-hazelle-mclean-51916b221" target="_blank">
+        <FaLinkedin size={20} color="#fffce2" />
+        </a> 
 				<span class="parameter">LinkedIn</span>
 			</div>
 			<div class="box">
-				<span class="value">1387</span>
+      <a href="https://www.behance.net/dhazelle" target="_blank">
+        <FaBehance size={20} color="#fffce2" />
+        </a>
 				<span class="parameter">Behance</span>
+        
 			</div>
 			<div class="box">
-				<span class="value">146</span>
+      <a href="https://github.com/Diante830" target="_blank">
+        <FaGithub size={20} color="#fffce2" />
+        </a>
 				<span class="parameter">Github</span>
 			</div>
 		</div>
--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Logo.css';
 
 const Logo = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div>
-      <img src="./profilepic.svg" alt="Profile Image" />
+    <div
+      className="logo-container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className={`image-layer top-layer ${isHovered ? 'hovered' : ''}`}>
+        <img src="./greyme.svg" alt="Profile Image" />
+      </div>
+      <div className={`image-layer bottom-layer ${isHovered ? 'hovered' : ''}`}>
+        <img src="./blueme.svg" alt="Profile Image" />
+      </div>
     </div>
   );
 };
